a5442e75bd4ff1c8c3bdad2f6aa3a2b8d954a4b3
archeana <anaclaudiasousajesus6@gmail.com>
2024-06-05 22:17:21 +0200

    add animation to the timeline

--- login-app/src/pages/HomePage.jsx
+++ login-app/src/pages/HomePage.jsx
@@ -37,38 +37,38 @@ const HomePage = () => {
           <div className="timeline">
             <div className="timecontainer left">
               <div className="content">
-                <h3>1. Create profile</h3>
-                <p>Register in our website for free. In just 2 steps.</p>
+                <h3>1. Create Profile</h3>
+                <p>Register on our website for free in just 2 steps.</p>
               </div>
             </div>
             <div className="timecontainer right">
               <div className="content">
-                <h3>2. Pick your language </h3>
+                <h3>2. Pick Your Language </h3>
                 <p>After signing in, you can select one or multiple official languages in Switzerland.</p>
               </div>
             </div>
             <div className="timecontainer left">
               <div className="content">
-                <h3>3. Pick your teacher</h3>
-                <p>In the matching page you can filter the teachers based on gender and canton. Now you have to wait for the teacher to accept.</p>
+                <h3>3. Pick Your Teacher</h3>
+                <p>On the matching page, you can filter teachers based on gender and canton. Then, you have to wait for the teacher to accept.</p>
               </div>
             </div>
             <div className="timecontainer right">
               <div className="content">
-                <h3>4. Teacher accepts</h3>
-                <p>The teacher will see your profile and will decide if he has time to be your buddly.</p>
+                <h3>4. Teacher Accepts</h3>
+                <p>The teacher will see your profile and decide if they have time to be your buddly.</p>
               </div>
             </div>
             <div className="timecontainer left">
               <div className="content">
-                <h3>5. You have now a language buddy</h3>
-                <p>If he accepts you will be abel to see his profile in your personal profile. You can now connect and learn your prefer language.</p>
+                <h3>5. You Have a Language Buddy</h3>
+                <p>If they accept, you will be able to see their profile in your personal profile. You can now connect and learn your preferred language.</p>
               </div>
             </div>
             <div className="timecontainer right">
               <div className="content">
-                <h3>Congrats!</h3>
-                <p>You will have access to teacher email and then can write, video chat or just meet up for a coffee.</p>
+                <h3>Congrats! 🎉</h3>
+                <p>You will have access to the teacher's email and can write, video chat, or just meet up for a coffee.</p>
               </div>
             </div>
           </div>
